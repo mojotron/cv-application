f7a1d05aa9444bda65efbd12959f048b2fd92f91
mojo <igormalj@gmail.com>
2024-01-14 12:11:33 +0100

    refactor drag and drop logic

--- src/components/Contact/ContactList.tsx
+++ src/components/Contact/ContactList.tsx
@@ -1,96 +1,95 @@
 import { IoTrashBin } from 'react-icons/io5';
-import { DragEvent, useState } from 'react';
+import { DragEvent, useState, useRef } from 'react';
 import { useCvStore } from '../../store';
 import ContactRow from './ContactRow';
-import { ContactOption } from '../../types/contactType';
 
 function ContactList() {
   const contact = useCvStore((state) => state.contact);
   const setContact = useCvStore((state) => state.setContact);
-
-  const [dragReorderActive, setDragReorderActive] = useState(false);
-  const [dragDeleteActive, setDragDeleteActive] = useState(false);
-
-  const [dropReorder, setDropReorder] = useState(false);
-  const [dropDelete, setDropDelete] = useState(false);
-  const [draggedContact, setDraggedContact] = useState<
-    null | keyof typeof ContactOption
-  >(null);
-
-  const handleDragStart = (contactName: string) => {
-    setDraggedContact(contactName as keyof typeof ContactOption);
+  // store index of dragged element
+  const [draggedItem, setDraggedItem] = useState<null | number>(null);
+  // store index of element draggedItem is over
+  const draggedOverItem = useRef<null | number>(null);
+  const [draggedOverDelete, setDraggedOverDelete] = useState(false);
+  // handlers reorder items
+  const handleDragStart = (e: DragEvent<HTMLLIElement>, index: number) => {
+    setDraggedItem(index);
   };
 
-  // STATE handlers
-  const handleDeleteContact = () => {
-    const newContact = contact.filter((c) => c.name !== draggedContact);
-    setContact(newContact);
-    setDraggedContact(null);
-  };
-
-  // REORDER handlers
-  const handleDragOverReorder = (e: DragEvent<HTMLUListElement>) => {
+  const handleDragEnter = (e: DragEvent<HTMLLIElement>, index: number) => {
     e.preventDefault();
-    setDragReorderActive(true);
+    if (draggedItem === null) return;
+    draggedOverItem.current = index;
+
+    const copyItems = [...contact];
+    const copyDraggedItem = { ...copyItems[draggedItem] };
+    copyItems.splice(draggedItem, 1);
+    copyItems.splice(draggedOverItem.current, 0, copyDraggedItem);
+    setDraggedItem(draggedOverItem.current);
+    draggedOverItem.current = null;
+    setContact(copyItems);
   };
 
-  const handleOnDropReorder = () => {
-    console.log('DROP reorder');
-    setDragReorderActive(false);
-  };
-
-  const handleDragLeaveReorder = (e: DragEvent<HTMLUListElement>) => {
+  const handleDragEnd = (e: DragEvent<HTMLLIElement>) => {
     e.preventDefault();
-    console.log('Drag leave');
+    setDraggedItem(null);
+    draggedOverItem.current = null;
   };
 
-  // DELETE handlers
-  const handleOnDragOverDelete = (e: DragEvent<HTMLDivElement>) => {
+  const handleDragOver = (e: DragEvent<HTMLLIElement | HTMLDivElement>) => {
+    e.stopPropagation();
     e.preventDefault();
-    setDragDeleteActive(true);
   };
-
-  const handleOnDragLeaveDelete = (e: DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDragDeleteActive(false);
-    setDropReorder(false);
+  // handlers delete items
+  const handleDragEnterDelete = () => {
+    setDraggedOverDelete(true);
   };
-
+  const handleDragLeaveDelete = () => {
+    setDraggedOverDelete(false);
+  };
   const handleOnDropDelete = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault();
-    e.stopPropagation();
-    handleDeleteContact();
-    setDraggedContact(null);
-    setDragDeleteActive(false);
+    if (draggedItem === null) return;
+    const id = contact[draggedItem].name;
+    const newContacts = contact.filter((c) => c.name !== id);
+    setContact(newContacts);
+    setDraggedOverDelete(false);
+    setDraggedItem(null);
+    draggedOverItem.current = null;
   };
 
   return (
-    <ul
-      onDragOver={handleDragOverReorder}
-      onDrop={handleOnDropReorder}
-      onDragLeave={handleDragLeaveReorder}
-      className="border border-3 border-slate-800 p-4 flex flex-col gap-3">
-      {contact.map((option) => (
-        <li
-          key={option.name}
-          className="border border-1 border-slate-600 cursor-move"
-          draggable
-          onDragStart={() => handleDragStart(option.name)}
-          id={option.name}>
-          <ContactRow data={option} />
-        </li>
-      ))}
-
+    <div className="border border-3 border-slate-800">
+      <ul className=" p-4 flex flex-col gap-3">
+        {contact.map((option, index) => (
+          <li
+            // DRAG handlers
+            draggable
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDragEnter={(e) => handleDragEnter(e, index)}
+            onDragEnd={handleDragEnd}
+            onDragOver={handleDragOver}
+            // - - - - - - - - - - - -
+            className="border border-1 border-slate-600 cursor-move"
+            key={option.name}
+            id={option.name}>
+            <ContactRow data={option} />
+          </li>
+        ))}
+      </ul>
       <div
-        className={`text-red-300 ml-auto ${
-          dragDeleteActive ? 'bg-red-400' : 'bg-transparent'
-        }`}
+        // DRAG handlers
+        onDragEnter={handleDragEnterDelete}
+        onDragLeave={handleDragLeaveDelete}
+        onDragOver={handleDragOver}
         onDrop={handleOnDropDelete}
-        onDragOver={handleOnDragOverDelete}
-        onDragLeave={handleOnDragLeaveDelete}>
+        // - - - - - - - - - - - -
+        className={`text-red-300 ml-auto border bg-blue-200 ${
+          draggedOverDelete ? 'bg-red-400' : 'bg-transparent'
+        }`}>
         <IoTrashBin size={40} />
       </div>
-    </ul>
+    </div>
   );
 }
 
--- src/components/Contact/ContactRow.tsx
+++ src/components/Contact/ContactRow.tsx
@@ -3,10 +3,10 @@ import { ContactType } from '../../types/contactType';
 
 function ContactRow({ data }: { data: ContactType }) {
   return (
-    <li className="flex gap-4 text-slate-600 text-sm items-center">
+    <div className="flex gap-4 text-slate-600 text-sm items-center">
       <span>{iconsConfig[data.name]}</span>
       <span className="w-full">{data.value}</span>
-    </li>
+    </div>
   );
 }
 
